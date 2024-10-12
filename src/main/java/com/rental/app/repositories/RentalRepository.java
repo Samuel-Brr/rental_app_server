27424d5f1727260ef40e5f9a91fb8ad2681b379c
Samuel-Brr <samuel.barre972@gmail.com>
2024-10-12 15:56:41 +0200

    RentalRepository
    removed unused methods

--- src/main/java/com/rental/app/repositories/RentalRepository.java
+++ src/main/java/com/rental/app/repositories/RentalRepository.java
@@ -1,15 +1,9 @@
 package com.rental.app.repositories;
 
 import com.rental.app.entities.Rental;
-import com.rental.app.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-    List<Rental> findByOwner(User owner);
-
-    List<Rental> findByPriceLessThanEqual(double maxPrice);
 }
