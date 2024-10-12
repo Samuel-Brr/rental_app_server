93bd47df65add65433865dc493aa66297821d22b
Samuel-Brr <samuel.barre972@gmail.com>
2024-10-12 16:03:07 +0200

    Implement MessageService for message management
    This commit introduces the MessageService class, which manages message-related operations:
    
    - Provides functionality for adding new messages to the system
    - Integrates with UserService and RentalService for entity relationships
    - Utilizes MessageRepository for data persistence
    - Implements DTO to entity mapping for message creation
    
    Key features:
    - Message creation with associated user and rental entities
    - Separation of concerns: Service layer handles business logic
    - Utilizes Mapper utility for DTO to entity conversion
    
    This service forms an essential part of our application's communication system, enabling users to send messages related to rental properties. It encapsulates message creation logic, ensuring proper association with users and rentals, and promotes a clean, maintainable architecture.

---
+++ src/main/java/com/rental/app/services/MessageService.java
@@ -0,0 +1,31 @@
+package com.rental.app.services;
+
+import com.rental.app.Utils.Mapper;
+import com.rental.app.dtos.MessageDto;
+import com.rental.app.entities.Message;
+import com.rental.app.entities.Rental;
+import com.rental.app.entities.User;
+import com.rental.app.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service responsible for message-related operations.
+ * This service handles the creation and storage of messages in the application.
+ */
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RentalService rentalService;
+
+    public Message addMessage(MessageDto messageDto) {
+        User user = userService.getUserById(messageDto.getUser_id());
+        Rental rental = rentalService.getRentalById(messageDto.getRental_id());
+        return messageRepository.save(Mapper.MapMessageDtoToMessage(messageDto, user, rental));
+    }
+}
