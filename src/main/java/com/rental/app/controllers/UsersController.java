44a1e7ef5698479e4df3b22c0cb24fc661c29597
Samuel-Brr <samuel.barre972@gmail.com>
2024-10-13 13:57:25 +0200

    Refactored UsersController to implement spring best practices

--- src/main/java/com/rental/app/controllers/UsersController.java
+++ src/main/java/com/rental/app/controllers/UsersController.java
@@ -9,8 +9,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,10 +24,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/user")
 @Tag(name = "Users", description = "User management API")
 @SecurityRequirement(name = "bearerAuth")
+@Validated
 public class UsersController {
 
-    @Autowired
-    private UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UsersController.class);
+
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
 
     @Operation(summary = "Get user by ID", description = "Retrieves a user's details by their ID")
     @ApiResponses(value = {
@@ -36,7 +44,15 @@ public class UsersController {
         @ApiResponse(responseCode = "404", description = "User not found")
     })
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        logger.info("Attempting to retrieve user with ID: {}", id);
+        try {
+            User user = userService.getUserById(id);
+            logger.info("Successfully retrieved user with ID: {}", id);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            logger.error("Unexpected error occurred while retrieving user with ID: {}", id, e);
+            throw new RuntimeException("An unexpected error occurred while retrieving the user", e);
+        }
     }
 }
